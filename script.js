c7aecf91ec5c874e5bf3915daf55275f64434411
Isaac Hussain <isaaclearns58@gmail.com>
2022-06-10 10:20:27 +0300

    Add operation functionality

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 class Calculator {
     constructor(previousOperandTextElement,currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     }
 
     clear() {
-
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = undefined;
     }
 
     delete() {  
@@ -13,7 +16,8 @@ class Calculator {
     }
 
     appendNumber(number) {
-
+        if (number === "." && this.currentOperand.includes(".")) return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
     chooseOperation(operation) {
@@ -25,7 +29,7 @@ class Calculator {
     }
 
     updateDisplay() {
-        
+        this.currentOperandTextElement.innerText = this.currentOperand;
     }
 
 
@@ -42,3 +46,21 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+
+    });
+});
